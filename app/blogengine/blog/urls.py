9bf62786e819e601e06e4561a827505cf2a0a1b2
Илья Симонов <issim91@outlook.com>
2018-12-08 18:19:49 +0400

    add new model - Tags

--- app/blogengine/blog/urls.py
+++ app/blogengine/blog/urls.py
@@ -18,5 +18,7 @@ from .views import *
 
 urlpatterns = [
     path('', posts_list, name='posts_list_url'),
-    path('post/<slug>/', post_detail, name='post_detail_url')
+    path('post/<slug>/', post_detail, name='post_detail_url'),
+    path('tags/', tags_list, name='tags_list_url'),
+    path('tag/<slug>/', tag_detail, name='tag_detail_url'),
 ]
