e2ff8fd0a1524945384891f0f3b3f8b3bebcd4ea
Илья Симонов <issim91@outlook.com>
2018-12-10 17:42:55 +0400

    add form delete tag and post

--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 from django.views.generic import View
+from django.urls import reverse
 
 from .models import Post, Tag
 from .utils import *
@@ -47,16 +48,13 @@ class PostUpdate(ObjectUpdateMixin, View):
     model_form = PostForm
     tamplate = 'blog/post_update_form.html'
     
-    # def get(self, request, slug):
-    #     tag = Tag.objects.get(slug__iexact=slug)
-    #     bound_form = TagForm(instance=tag)
-    #     return render(request, 'blog/tag_update_form.html', context={'form': bound_form, 'tag': tag})
 
+class TagDelete(ObjectDeleteMixin, View):
+    model = Tag
+    template = 'blog/tag_delete_form.html'
+    redirect_url = 'tags_list_url'
 
-    # def post(self, request, slug):
-    #     tag = Tag.objects.get(slug__iexact=slug)
-    #     bound_form = TagForm(request.POST, instance=tag)
-    #     if bound_form.is_valid():
-    #         new_tag = bound_form.save()
-    #         return redirect(new_tag)
-    #     return render(request, 'blog/tag_update_form.html', context={'form': bound_form, 'tag': tag})
+class PostDelete(ObjectDeleteMixin, View):
+    model = Post
+    template = 'blog/post_delete_form.html'
+    redirect_url = 'posts_list_url'
